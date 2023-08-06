8aaf40442844e14162312164fed6fb24616d98d7
isaac <isaac@Isaac341>
2023-08-06 23:55:33 +0800

    teach me senpai (isaac)

--- C206_CaseStudy/src/Teacher.java
+++ C206_CaseStudy/src/Teacher.java
@@ -1,5 +1,28 @@
 // Isaac
 
 public class Teacher {
-
+	
+	private String teacherName;
+	private String assignedCourses;
+	
+	public Teacher(String teacherName) {
+		this.teacherName = teacherName;
+		this.assignedCourses = "";
+	}
+	public Teacher(String teacherName,String assignedCourses) {
+		this.teacherName = teacherName;
+		this.assignedCourses = assignedCourses;
+	}
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public String getAssignedCourses() {
+		return assignedCourses;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public void setAssignedCourses(String assignedCourses) {
+		this.assignedCourses = assignedCourses;
+	}
 }
