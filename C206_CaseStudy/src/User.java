ec4a911204d075a506428b56df6a77a371ff999e
isaac <isaac@10.175.6.101>
2023-08-07 11:37:31 +0800

    user superclass

---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,36 @@
+//Isaac
+//SuperClass for all users
+public class User {
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {		
+	this.username = username;
+	this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void showInfo( ) {
+		System.out.println("Username: " + username);
+		System.out.println("Password: " + password);
+	}
+
+	
+
+
+}
